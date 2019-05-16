ca133797db84af0cbda7ca20755010d8366cf0f3
Ruben Musalia <grande.maitre@gmial.com>
2019-05-16 16:56:40 +0300

    Removed some code

--- oopsetup.py
+++ oopsetup.py
@@ -1,18 +1,11 @@
-import sys
 import requests
-import os
-import math
+
+r = requests.get("https://coreyms.com")
+
+print(r.status_code)
+print(r.ok)
 
 
 
-# r = requests.get("https://coreyms.com")
-
-# print(r.status_code)
-# print(sys.version)
-# print(sys.executable)
-
-
-name = input("Your name ")
-print("Hello " + name )
 
 
