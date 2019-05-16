2dc8a5ab5f70066ee2ff1f6706de170a05c49fd4
Ruben Musalia <grande.maitre@gmial.com>
2019-05-16 16:53:07 +0300

    initial commit

---
+++ oopsetup.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+import os
+import math
+
+
+
+# r = requests.get("https://coreyms.com")
+
+# print(r.status_code)
+# print(sys.version)
+# print(sys.executable)
+
+
+name = input("Your name ")
+print("Hello " + name )
+
+
